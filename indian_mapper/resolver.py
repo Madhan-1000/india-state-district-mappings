3eefb4e254f20b4d0d292464f9998053f736e349
ATURU GURU MADHAN REDDY <MADHANREDDY.exe@gmail.com>
2026-01-29 08:23:10 +0530

    Added all the fucntions that needs to be in Library.

--- indian_mapper/resolver.py
+++ indian_mapper/resolver.py
@@ -1,2 +1,10 @@
-def resolve_district(district_name:str):
+import json
+
+def resolve_district_to_state(district_name:str)->str:
+    pass
+
+def canonical_name_for_district(district_name:str)->str:
+    pass
+
+def canonical_name_for_state(state_name:str)->str:
     pass
